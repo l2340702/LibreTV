a28f8b0f1c1844c7f7e70b65ab1f9fa4637f4620
l2340702 <l2340702@163.com>
2026-06-26 02:03:45 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,7 @@
 const CUSTOMER_SITES = {
     qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+        api: 'https://155api.com/api.php/provide/vod/at/json/',
+        name: '黄色资源',
     }
 };
 
